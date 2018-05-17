cc9a8ee66fb6d12966a2800a28c997d7bf9c698f
Jason Ritenour <jritenou@redhat.com>
2018-05-17 12:06:14 -0400

    added initial GUI interaction playbook & files

---
+++ winpython/notepadstart.py
@@ -0,0 +1,6 @@
+from pywinauto.application import Application
+app = Application().start("notepad.exe")
+
+app.UntitledNotepad.menu_select("Help->About Notepad")
+app.AboutNotepad.OK.click()
+app.UntitledNotepad.Edit.type_keys("pywinauto Works!", with_spaces = True)
